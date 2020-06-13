d839af4c210c2c36fd6d4ce17f5f48da1ad23c2b
Gabriel Juchault <gabriel.juchault@gmail.com>
2020-06-13 11:22:08 +0200

    feat: add receiver sensors config

--- src/server/http/controllers/sensors.ts
+++ src/server/http/controllers/sensors.ts
@@ -1,8 +1,14 @@
 import { Request, Response } from 'express'
-import { sensors } from '../services/sensors'
+import { listSensorValues, listReceiverSensors } from '../services/sensors'
 
 export async function handleSensors(req: Request, res: Response) {
-  const lastSensorsValues = await sensors()
+  const lastSensorsValues = await listSensorValues()
 
   res.status(200).json(lastSensorsValues)
+}
+
+export async function handleListReceiverSensors(req: Request, res: Response) {
+  const receiverSensors = await listReceiverSensors()
+
+  res.status(200).json(receiverSensors)
 }
