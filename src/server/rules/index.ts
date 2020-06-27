cfc4abc5fdc6f0de12374e596c40f6ea46d991fd
Gabriel Juchault <gabriel.juchault@gmail.com>
2020-06-27 18:55:58 +0200

    fix: Actionables & Sensors

--- src/server/rules/index.ts
+++ src/server/rules/index.ts
@@ -62,6 +62,8 @@ export async function createRules({ storage }: { storage: Storage }) {
         date: now,
         emitterSensors,
         actionables,
+        Actionables,
+        Sensors,
       })
 
       try {
