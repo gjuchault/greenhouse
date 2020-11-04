e0e92966c3d1a92be7205c88c4f655701c4c8128
Gabriel Juchault <gabriel.juchault@gmail.com>
2020-11-04 22:59:34 +0100

    feat(tables): confirm removal

--- src/app/components/ActionablesTable/ActionablesTable.tsx
+++ src/app/components/ActionablesTable/ActionablesTable.tsx
@@ -1,59 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Menu } from 'evergreen-ui'
 import { Actionable } from '../../hooks/useQuery'
 import { Table, makeDateCell } from '../Table/Table'
+import { Confirm } from '../Confirm/Confirm'
 
 type Props = {
   actionables: Actionable[]
-  onRemoveActionable: (actionableId: string) => void
+  onRemoveActionable: (actionableId: string) => Promise<void>
 }
 
 export function ActionablesTable({ actionables, onRemoveActionable }: Props) {
+  const [actionableToDelete, setActionableToDelete] = useState<
+    Actionable | undefined
+  >(undefined)
+  const [isRemoving, setIsRemoving] = useState<boolean>(false)
+
   return (
-    <Table<Actionable>
-      items={actionables}
-      renderFilterPlaceholder={(count) =>
-        `Rechercher parmis ${count} actionnables`
-      }
-      columnsSizes={['auto', 150, 150, 150, 180]}
-      columns={[
-        {
-          Header: 'Nom',
-          accessor: 'name',
-        },
-        {
-          Header: 'Adresse',
-          accessor: 'target',
-        },
-        {
-          Header: 'Type de valeur',
-          accessor: (item) => item.valueType.range,
-        },
-        {
-          Header: 'Valeur actuelle',
-          accessor: (item) => item.lastAction?.value,
-        },
-        {
-          Header: 'Envoyée le',
-          accessor: (item) => new Date(item.lastAction?.sentAt ?? 0),
-          ...makeDateCell(),
-        },
-      ]}
-      renderMenu={(actionable, close) => (
-        <Menu>
-          <Menu.Group>
-            <Menu.Item
-              intent="danger"
-              onClick={() => {
-                onRemoveActionable(actionable.id)
-                close()
-              }}
-            >
-              Supprimer
-            </Menu.Item>
-          </Menu.Group>
-        </Menu>
+    <>
+      {actionableToDelete && (
+        <Confirm
+          description={`Supprimer l'actionnable ${actionableToDelete.name}?`}
+          isLoading={isRemoving}
+          onClose={() => {
+            setActionableToDelete(undefined)
+          }}
+          onConfirm={async () => {
+            setIsRemoving(true)
+            await onRemoveActionable(actionableToDelete.id)
+            setIsRemoving(false)
+            setActionableToDelete(undefined)
+          }}
+        />
       )}
-    />
+      <Table<Actionable>
+        items={actionables}
+        renderFilterPlaceholder={(count) =>
+          `Rechercher parmis ${count} actionnables`
+        }
+        columnsSizes={['auto', 150, 150, 150, 180]}
+        columns={[
+          {
+            Header: 'Nom',
+            accessor: 'name',
+          },
+          {
+            Header: 'Adresse',
+            accessor: 'target',
+          },
+          {
+            Header: 'Type de valeur',
+            accessor: (item) => item.valueType.range,
+          },
+          {
+            Header: 'Valeur actuelle',
+            accessor: (item) => item.lastAction?.value,
+          },
+          {
+            Header: 'Envoyée le',
+            accessor: (item) => new Date(item.lastAction?.sentAt ?? 0),
+            ...makeDateCell(),
+          },
+        ]}
+        renderMenu={(actionable, close) => (
+          <Menu>
+            <Menu.Group>
+              <Menu.Item
+                intent="danger"
+                onClick={() => {
+                  setActionableToDelete(actionable)
+                  close()
+                }}
+              >
+                Supprimer
+              </Menu.Item>
+            </Menu.Group>
+          </Menu>
+        )}
+      />
+    </>
   )
 }
